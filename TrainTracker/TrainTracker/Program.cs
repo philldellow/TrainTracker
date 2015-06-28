d1714f86251100b6d38058618125fd50bdc7634b
philldellow <philldellow@gmail.com>
2015-06-28 12:22:14 +1200

    hello world, response complete

--- TrainTracker/TrainTracker/Program.cs
+++ TrainTracker/TrainTracker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace TrainTracker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello and Welcome");
+            Console.WriteLine("To view this menu at any time, enter 'Help'");
+            string mainmenuresponse = Console.ReadLine();
+            Console.WriteLine(mainmenuresponse);
+            Console.ReadKey();
+
         }
     }
 }
